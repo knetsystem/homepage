d63c74cacc539bfbb6639bc9509b543e8a8d1cb1
Emil Kristensen <eKristensen@users.noreply.github.com>
2024-11-17 00:50:37 +0100

    Redirect to disable pages.dev domain
    Signed-off-by: Emil Kristensen <eKristensen@users.noreply.github.com>

--- _worker.js
+++ _worker.js
@@ -2,13 +2,14 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    // Redirect www to root domain
-    if (url.hostname.startsWith("www.")) {
-      url.hostname = url.hostname.replace("www.", "");
+    // Redirect to https://k-net.dk if hostname is not k-net.dk or if protocol is not HTTPS
+    if (url.hostname !== "k-net.dk" || url.protocol !== "https:") {
+      url.hostname = "k-net.dk";
+      url.protocol = "https:";
       return Response.redirect(url.toString(), 301); // Permanent redirect
     }
 
-    // Continue serving the request for all other routes
+    // Continue serving the request for valid hostnames and HTTPS protocol
     return env.ASSETS.fetch(request);
   },
 };
